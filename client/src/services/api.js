2eb52d68ba3b9c7296cc2bbb2d91a20f19e1ee75
mouse <kishorkumarl9088@gmail.com>
2026-05-28 02:00:40 +0530

    Fixed API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "https://smart-expense-tracker-mo5w.onrender.com/api",
+    " https://smart-expense-tracker-mo5w.onrender.com/api",
 });
 
 export default API;

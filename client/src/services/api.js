afd464b0f0e8bc0f1ee3eaa3bdbcc5d890bdd306
mouse <kishorkumarl9088@gmail.com>
2026-05-28 01:10:00 +0530

    Connected deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    "https://smart-expense-tracker-mo5w.onrender.com/api",
 });
 
 export default API;
